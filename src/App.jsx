b490578c995a5777200d4a8385c88d7ef27e5ec9
Rosenma1 <160132267+Rosenma1@users.noreply.github.com>
2026-09-20 16:18:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,175 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div>
+      
+<section class="header">
+    <nav>
+        <a href=""><img src="/Users/mac/Downloads/eduford_img/logo.png"alt="Graduation cap logo" /></a>
+        <div class="nav-links">
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">ABOUT</a></li>
+                <li><a href="">COURSE</a></li>
+                <li><a href="">BLOG</a></li>
+                <li><a href="">CONTACT US</a></li>
+            </ul>
+        </div>
+    
+    </nav>
+     
+      <div class="hero">
+         <h2>World's Biggest University</h2>
+         <p>Making website is one of the easiest things in the world. You just need to learn Html, Css, <br/>Javascript and you are good to go</p>
+         <a href="" class="hero-btn">Visit us to Know more</a>
+        
+        </div>
+
+      
+
+</section>
+  <section class="course">  
+    <h1>Courses we Offer</h1> 
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, sequi.</p>
+    
+   <div class="row">
+    <div class="course-col">
+        <h3>Intermediate</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde mollitia, ducimus accusantium ab porro doloremque odit recusandae harum aut officia.</p>
+
+    </div>
+    <div class="course-col">
+        <h3>Degree</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde mollitia, ducimus accusantium ab porro doloremque odit recusandae harum aut officia.</p>
+
+    </div>
+    <div class="course-col">
+        <h3>Post Graduation</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde mollitia, ducimus accusantium ab porro doloremque odit recusandae harum aut officia.</p>
+
+    </div>
+
+
+   </div>
+    
+</section>
+
+
+  <section class="campus">
+    <h1>Our Global Campus</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda officia eius dignissimos ex illum id?</p>
+     <div class="row">
+        <div class="campus-col">
+            <img src="/Users/mac/Downloads/eduford_img/london.png" alt="" />
+            <div class="layer">
+                <h3>LONDON</h3>
+            </div>
+        </div>
+        <div class="campus-col">
+            <img src="/Users/mac/Downloads/eduford_img/newyork.png" alt="" />
+            <div class="layer">
+                <h3>NEWYORK</h3>
+            </div>
+            </div>
+        <div class="campus-col">
+            <img src="/Users/mac/Downloads/eduford_img/washington.png" alt="" />
+            <div class="layer">
+                <h3>WASHINGTON</h3>
+            </div>
+        </div>
+     </div>
+  </section>
+
+{/* <!-- Facilities --> */}
+
+<section class="facilities">
+    <h1>Our Facilities</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, cupiditate.</p>
+     <div class="row">
+        <div class="facilities-col">
+            <img src="/Users/mac/Downloads/eduford_img/library.png" alt="library" />
+            <h3>World Class Library</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptates, necessitatibus vero odio obcaecati voluptatibus.</p>
+        </div>
+     
+        <div class="facilities-col">
+            <img src="/Users/mac/Downloads/eduford_img/basketball.png" alt="basketball" />
+            <h3>Largest Play Ground</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptates, necessitatibus vero odio obcaecati voluptatibus.</p>
+        </div>
+     
+        <div class="facilities-col">
+            <img src="/Users/mac/Downloads/eduford_img/cafeteria.png" alt="Cafeteria" />
+            <h3>Tasty and Healthy Food</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptates, necessitatibus vero odio obcaecati voluptatibus.</p>
+        </div>
+    </div>
+
+  </section>
+
+  {/* <!-- TESTIMONIALS --> */}
+  <section class="testimonials">
+    <h1>What Our Students Say</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero consequuntur ratione explicabo, ex inventore nesciunt nobis numquam at alias exercitationem hic laudantium expedita officia animi?</p>
+
+    <div class="row">
+        <div class="testimonial-col">
+            <img src="../../../../Downloads/eduford_img/user1.jpg" alt="Student1" />
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, iure. Illum delectus natus eaque iusto, eum velit at tempora totam optio beatae asperiores omnis voluptatem unde sit iure eos ipsa?</p>
+                <h3>Christine Berkley</h3>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+                
+            </div>
+
+        </div>
+        <div class="testimonial-col">
+            <img src="../../../../Downloads/eduford_img/user2.jpg" alt="Student2" />
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, iure. Illum delectus natus eaque iusto, eum velit at tempora totam optio beatae asperiores omnis voluptatem unde sit iure eos ipsa?</p>
+                <h3>David Byer</h3>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+            </div>
+
+        </div>
+
+    </div>
+
+  </section>
+
+  {/* <!-- call To Action --> */}
+
+  <section class="cta">
+    <h1>Enroll for our various online classes anywhere from the world</h1>
+    <a href="" class="hero-btn">CONTACT US</a>
+  </section>
+
+  {/* <!-- Footer --> */}
+
+  <section class="footer">
+    <h4>About Us</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse necessitatibus qui eos officia dolorem eaque aliquam adipisci, itaque porro soluta nisi quam mollitia, modi distinctio, <br/>alias vitae magni ea eligendi delectus dolores quos? Laborum adipisci fugiat animi, voluptatem a incidunt.</p>
+     <div class="icons">
+        <i class="fa fa-facebook"></i>
+        <i class="fa fa-twitter"></i>
+        <i class="fa fa-instagram"></i>
+        <i class="fa fa-linkedin"></i>
+     </div>
+     <p>Made with <i class="fa fa-heart-o"></i> by Nma</p>
+
+
+  </section>
+    </div>
+  )
+}
+
+export default App
